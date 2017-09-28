199257da997c2501e215cc5394e89adc60959339
jinga4x <jainigo@profile.es>
2017-09-28 11:26:25 +0200

    pro

--- src/com/codependent/jenkins/pipelines/openshift/Utils.groovy
+++ src/com/codependent/jenkins/pipelines/openshift/Utils.groovy
@@ -1,7 +1,7 @@
 package com.codependent.jenkins.pipelines.openshift
 
 static def promoteAndVerify(project, sourceTag, destinationTag, sourceNamespace, destinationNamespace){
-  echo "Tagging Image $project:$sourceTag -> $project:$destinationTag - $sourceNamespace"
+  echo "Tagging"
   openshiftTag(srcStream: project, srcTag: sourceTag, destStream: project, destTag: destinationTag, namespace: sourceNamespace)
   echo 'Verifying deployment'
   openshiftVerifyDeployment(namespace: destinationNamespace, depCfg: project)
